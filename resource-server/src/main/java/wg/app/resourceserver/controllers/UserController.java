3816961ae4bfcb2ec46ed0ecf6fd39f0b73450ce
WojciechGalecki <wojciech.galecki@wp.pl>
2021-03-28 14:50:43 +0200

    method level security example

--- resource-server/src/main/java/wg/app/resourceserver/controllers/UserController.java
+++ resource-server/src/main/java/wg/app/resourceserver/controllers/UserController.java
@@ -1,8 +1,12 @@
 package wg.app.resourceserver.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+import wg.app.resourceserver.response.UserRest;
 
 @RestController
 @RequestMapping("/users")
@@ -11,5 +15,18 @@ public class UserController {
     @GetMapping("/status")
     public String status() {
         return "Working...";
+    }
+
+    //@Secured("ROLE_developer")
+    @PreAuthorize("hasRole('developer')") // "hasAuthority('ROLE_developer') or #id == #jwt.subject"
+    @DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
+    }
+
+    @PostAuthorize("returnObject.userId == #jwt.subject")
+    @GetMapping(path = "/{id}")
+    public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return new UserRest("John", "Doe", "");
     }
 }
